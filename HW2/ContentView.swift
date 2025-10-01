52e1c2ac68163c9d7dc38fd945019fd4fe08473c
matt <matt>
2025-10-01 10:38:07 +0800

    init

---
+++ HW2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HW2
+//
+//  Created by user19 on 2025/10/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HW2/HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  HW2App.swift
+//  HW2
+//
+//  Created by user19 on 2025/10/1.
+//
+
+import SwiftUI
+
+@main
+struct HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
